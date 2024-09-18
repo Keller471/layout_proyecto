8a86c63b44b3c649a3120f6d661ccc8883732805
Keller471 <70558322+Keller471@users.noreply.github.com>
2024-09-17 21:31:17 -0300

    restaurante update

--- js/leerresto.js
+++ js/leerresto.js
@@ -9,17 +9,20 @@ fetch('js/restos.json')
             if (e.name == localStorage.getItem("titulo")) {
                 body.innerHTML += 
                     ` <header class="restaurante">
-                        <a href="index.html" class="iconos">
+                        <a href="restaurantes.html" class="iconos">
         arrow_back
     </a>
 <section class="portada">
 
                 <img src="${e.avatar.src}" class="imagen">
 </section>
+
+</header>
 <section class="informacion">
     <div><br>
         <h2>${e.name}</h2>
         <p>${e.location} </p>
+    <p>${e.description}</p>
     </div>
     <div class="valor">
     ${`<span class="iconos verde f13">euro_symbol</span>`.repeat(e.price)}
@@ -28,39 +31,108 @@ fetch('js/restos.json')
     <div class="calificacion">
         ${e.rating}
     </div>
+    
 </section>
-</header>
 <main class="index">
-<section class="datos">
-    <div class="icon">
-        <span class="iconos f25 ">
-            alarm_on
-        </span>
-        <p>
-        ${e.open} - ${e.close}
-        </p>
-    </div>
-    <div class="icon">
-        <span class="iconos f25 ">
-            pin_drop
-        </span>
-        <p>
-        ${e.distance}
-        </p>
-    </div>
-    <div class="icon">
-        <span class="iconos f25 ">
-            delivery_dining
-        </span>
-        <p>
-            DELIVERY
-        </p>
-    </div>
-</section>
-<div class="descripcion-restaurante">
-    <p>${e.description}</p>
-</div>
+<nav class="categorias">
+            <img src="${e.avatar.src}" class="imagen-resto">
+            <img src="${e.avatar.src}" class="imagen-resto">
+            <img src="${e.avatar.src}" class="imagen-resto">
+            <img src="${e.avatar.src}" class="imagen-resto">
+        </nav>
+
 </main>
+<div class="menu">
+<nav class="categorias1">
+            <button class="btn_categoria1">
+
+            </button>
+            <button class="btn_categoria1">
+
+            </button>
+            <button class="btn_categoria1">
+
+            </button>
+            <button class="btn_categoria1">
+
+            </button>
+            <button class="btn_categoria1">
+
+            </button>
+        </nav>
+        <div class="container">
+        <div class="item">
+            <div>Chocolate tart</div>
+            <div>$6.30</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+        <div class="item">
+            <div>Cheesecake</div>
+            <div>$5.68</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+        <div class="item">
+            <div>Strawberry cake</div>
+            <div>$8.49</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+        <div class="item">
+            <div>Strawberry cake</div>
+            <div>$8.49</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+        <div class="item">
+            <div>Strawberry cake</div>
+            <div>$8.49</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+        <div class="item">
+            <div>Strawberry cake</div>
+            <div>$8.49</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+        <div class="item">
+            <div>Strawberry cake</div>
+            <div>$8.49</div>
+            <button>
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.99996 10.75L10.75 5.99996" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.75 5.99996L10.75 10.75" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+        </div>
+    </div>
+</div>
 `}
     });
     })
